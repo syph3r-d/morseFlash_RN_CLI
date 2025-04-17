9ef15b525fbb5d375eb22d3165b900e894fc2861
Sadeepa Dhananjaya <115467947+syph3r-d@users.noreply.github.com>
2025-04-17 13:00:43 +0530

    feat: update minimum SDK version, add new dependencies, and implement camera functionality in Receive view

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
+  plugins: [
+    ['react-native-worklets-core/plugin'],
+    'react-native-reanimated/plugin',
+  ],
 };
--- views/Receive.tsx
+++ views/Receive.tsx
@@ -1,6 +1,46 @@
+import {Skia} from '@shopify/react-native-skia';
 import React from 'react';
 import {Text} from 'react-native-paper';
+import {
+  Camera,
+  useCameraDevice,
+  useFrameProcessor,
+  useSkiaFrameProcessor,
+} from 'react-native-vision-camera';
 
 export const Receive = () => {
-  return <Text>Receive</Text>;
+  const device = useCameraDevice('back');
+  //   const frameProcessor = useFrameProcessor(frame => {
+  //     'worklet';
+  //     console.log('Frame processed');
+  //     console.log(`Frame width: ${frame.width}, height: ${frame.height}`);
+  //     console.log(`Frame pixel format: ${frame.pixelFormat}`);
+
+  //     if (frame.pixelFormat === 'yuv') {
+  //       const buffer = frame.toArrayBuffer();
+  //       const data = new Uint8Array(buffer);
+  //       console.log(`Pixel at 0,0: YUV(${data[0]}, ${data[1]}, ${data[2]})`);
+  //     }
+  //   }, []);
+  const frameProcessor = useSkiaFrameProcessor(frame => {
+    'worklet';
+    frame.render();
+
+    const centerX = frame.width / 2;
+    const centerY = frame.height / 2;
+    const rect = Skia.XYWHRect(centerX, centerY, 150, 150);
+    const paint = Skia.Paint();
+    paint.setColor(Skia.Color('red'));
+    frame.drawRect(rect, paint);
+  }, []);
+
+  if (device == null) return <Text>Loading...</Text>;
+  return (
+    <Camera
+      style={{flex: 1}}
+      device={device}
+      isActive={true}
+      frameProcessor={frameProcessor}
+    />
+  );
 };
